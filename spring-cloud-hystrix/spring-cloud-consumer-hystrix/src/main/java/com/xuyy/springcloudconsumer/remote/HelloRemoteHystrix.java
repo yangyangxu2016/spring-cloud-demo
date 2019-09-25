e38b166a52b1a294110fa2544c1c28a4c5bb6fbe
DESKTOP-DRBVSBP\xuyy <1425809544@qq.com>
2019-09-25 16:11:59 +0800

    服务消费者：9001，支持熔断 hystrix

---
+++ spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/xuyy/springcloudconsumer/remote/HelloRemoteHystrix.java
@@ -0,0 +1,16 @@
+package com.xuyy.springcloudconsumer.remote;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author xuyangyang
+ */
+@Component
+public class HelloRemoteHystrix implements HelloRemote{
+
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello" +name+", this messge send failed ";
+    }
+}
